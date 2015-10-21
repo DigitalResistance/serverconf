fc4f110fe9bc80de4d165ef5fde47df7847388da
Alexander Steers <alex.steers@gmail.com>
2015-10-20 21:56:54 -0400

    Deadliest Warrior map rotation, and server CPU affinity
    We were seeing random server crashes running in a 8-core VM, due to
    negative delta time. Might be due to CPU timing sensitivity in UDK that
    gets funky in virtualbox. I tried setting the CPU affinity to a single
    core (each) which seems to be better maybe?

--- chivalry_dw/chivalry_dw_server.bat
+++ chivalry_dw/chivalry_dw_server.bat
@@ -6,4 +6,5 @@ set ADMIN_PW=LanTestAdmin
 set CHIVPORT=7877
 set STEAMPORT=27815
 rem Launch the server... it might take a second
-start "Chivalry:DW Server" "%CHIV_DW_DED%\Binaries\Win64\CDWLogging.exe" "TD-Bamboo_small?game=CDW.AOCTD?numteams=2?class0=Open?class1=Open?steamsockets?AdminPassword=%ADMIN_PW%" -Port=%CHIVPORT% -QueryPort %STEAMPORT% -log=LanTest\server.log -configsubdir=LanTest -seekfreeloadingserver
+rem Set processor affinity when running on a multi-core VM to avoid negative delta time crashes
+start /affinity 6 "Chivalry:DW Server" "%CHIV_DW_DED%\Binaries\Win64\CDWLogging.exe" "LTS-Frigid?game=CDW.LTS?numteams=4?class0=Open?class1=Open?class2=Open?class3=Open?steamsockets?AdminPassword=%ADMIN_PW%" -Port=%CHIVPORT% -QueryPort %STEAMPORT% -log=LanTest\server.log -configsubdir=LanTest -seekfreeloadingserver
--- chivalry_mw/chivalry_mw_server.bat
+++ chivalry_mw/chivalry_mw_server.bat
@@ -6,4 +6,5 @@ set ADMIN_PW=LanTestAdmin
 set CHIVPORT=7777
 set STEAMPORT=27015
 rem Launch the server... it might take a second
-start "Chivalry:MW Server" "%CHIV_MW_DED%\Binaries\Win64\UDKLogging.exe" "AOCTD-Forest-CM_p?steamsockets?AdminPassword=%ADMIN_PW%?modname=BlackKnight" -sdkfileid=232823090 -Port=%CHIVPORT% -QueryPort %STEAMPORT% -log=LanTest\server.log -configsubdir=LanTest -seekfreeloadingserver
+rem Set processor affinity when running on a multi-core VM to avoid negative delta time crashes
+start /affinity 8 "Chivalry:MW Server" "%CHIV_MW_DED%\Binaries\Win64\UDKLogging.exe" "AOCTD-Forest-CM_p?steamsockets?AdminPassword=%ADMIN_PW%?modname=BlackKnight" -sdkfileid=232823090 -Port=%CHIVPORT% -QueryPort %STEAMPORT% -log=LanTest\server.log -configsubdir=LanTest -seekfreeloadingserver
